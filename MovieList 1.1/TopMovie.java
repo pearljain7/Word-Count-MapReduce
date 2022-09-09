4cb6f63a6ddbfbb8d4cd26c3c75760d10c7ca2e2
Pearl <pearljainv@gmail.com>
2022-09-09 17:19:08 -0400

    Adding analysis and modified files

--- MovieList 1.1/TopMovie.java
+++ MovieList 1.1/TopMovie.java
@@ -27,21 +27,12 @@ public class TopMovie {
         public void map(Object key, Text value, Context context)
                 throws IOException, InterruptedException {
 
-            // input data format => movie_name
-            // no_of_views (tab separated)
-            // we split the input data
             String[] tokens = value.toString().split("\t");
 
             String movie_name = tokens[0];
             long no_of_views = Long.parseLong(tokens[1]);
-
-            // insert data into treeMap,
-            // we want top 10 viewed movies
-            // so we pass no_of_views as key
             tmap.put(no_of_views, movie_name);
 
-            // we remove the first key-value
-            // if it's size increases 10
             if (tmap.size() > 10) {
                 tmap.remove(tmap.firstKey());
             }
@@ -75,10 +66,6 @@ public class TopMovie {
     public void reduce(Text key, Iterable<LongWritable> values,
       Context context) throws IOException, InterruptedException
     {
- 
-        // input data from mapper
-        // key                values
-        // movie_name         [ count ]
         String name = key.toString();
         long count = 0;
  
@@ -86,14 +73,8 @@ public class TopMovie {
         {
             count = val.get();
         }
- 
-        // insert data into treeMap,
-        // we want top 10 viewed movies
-        // so we pass count as key
         tmap2.put(count, name);
  
-        // we remove the first key-value
-        // if it's size increases 10
         if (tmap2.size() > 10)
         {
             tmap2.remove(tmap2.firstKey());
@@ -120,8 +101,6 @@ public static void main(String[] args) throws Exception
     Configuration conf = new Configuration();
     String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
 
-    // if less than two paths
-    // provided will show error
     if (otherArgs.length < 2)
     {
         System.err.println("Error: please provide two paths");
